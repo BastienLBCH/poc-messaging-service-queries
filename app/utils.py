9e47ff7f9d36c17a96608b1fa96bcc1ad22b59cf
BastienLBCH <32386403+BastienLB@users.noreply.github.com>
2023-09-22 12:15:44 +0200

    added new function

--- app/utils.py
+++ app/utils.py
@@ -5,7 +5,7 @@ from fastapi import Request
 
 def get_userid_from_request(request: Request):
     """
-    Extract the userid from the token
+    Extract the userid from the token inside the request
     :param request: Fastapi Request
     :return: str user id
     """
@@ -13,3 +13,10 @@ def get_userid_from_request(request: Request):
     return jwt.decode(token, options={"verify_signature": False})["sub"]
 
 
+def get_userid_from_token(token: str):
+    """
+    Extract the userid from the token
+    :param token: str JWT
+    :return: str user id
+    """
+    return jwt.decode(token, options={"verify_signature": False})["sub"]
